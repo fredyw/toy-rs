b5b505ff8dd29a8d6fc48242b2876ce48add8784
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-23 18:35:26 -0600

    Add simple AST

---
+++ src/ast.rs
@@ -0,0 +1,41 @@
+#[derive(Debug, Clone)]
+pub enum Literal {
+    Int(i64),
+    Bool(bool),
+    Str(String),
+}
+
+#[derive(Debug, Clone)]
+pub enum BinaryOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Lt,
+    Gt,
+    Eq,
+}
+
+#[derive(Debug, Clone)]
+pub enum UnaryOp {
+    Neg,
+    Not,
+}
+
+#[derive(Debug, Clone)]
+pub enum Expr {
+    Literal(Literal),
+    Binary(Box<Expr>, BinaryOp, Box<Expr>),
+    Unary(UnaryOp, Box<Expr>),
+    Variable(String),
+    Call(String, Vec<Expr>),
+    Block(Vec<Stmt>),
+    If(Box<Expr>, Box<Expr>, Option<Box<Expr>>),
+}
+
+#[derive(Debug, Clone)]
+pub enum Stmt {
+    Let(String, Expr),
+    Fn(String, Vec<String>, Vec<Stmt>),
+    Expression(Expr),
+}
